379d9d71861a5752b9ed1a1cb0faa0e3ff5bbe3d
ashfaqashraf <ashfaq.ashraf@gmail.com>
2025-11-05 12:06:16 +0530

    Create questions.js

---
+++ src/questions.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    text: "Iron Man has 2 arc reactors and gets 3 more from his workshop. How many arc reactors does he have now?",
+    options: ["3", "5", "6", "2"],
+    answer: "5",
+    type: "mcq",
+  },
+  {
+    text: "Spider-Man swings past 4 buildings and sees 6 more ahead. How many buildings will he pass?",
+    options: ["9", "10", "8", "7"],
+    answer: "10",
+    type: "mcq",
+  },
+  {
+    text: "Captain America trains with 7 Avengers. If 3 more join, how many Avengers are there in total?",
+    options: ["9", "10", "7", "5"],
+    answer: "10",
+    type: "mcq",
+  },
+  {
+    text: "There are 8 shields in the Avengers Tower. Captain America gives away 3 shields. How many are left?",
+    options: ["3", "7", "5", "4"],
+    answer: "5",
+    type: "mcq",
+  },
+  {
+    text: "Describe in your own words: Which Marvel hero do you want to help with math, and what math power would you give them?",
+    type: "desc",
+    answer: "",
+  },
+];
+
+export default questions;
